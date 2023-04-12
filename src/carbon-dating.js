f2f9dde893916c8d63e78fcec80aab366e110118
Ilya Tupalski <itupalski@gmail.com>
2023-04-12 11:56:43 +0400

    feat: carbon-dating (Wed, Apr 12, 2023 11:56:43 AM)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,10 @@ const { NotImplementedError } = require('../extensions/index.js');
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
+function isNumber(num) {
+	return typeof num === 'number' && !isNaN(num);
+}
+
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -16,11 +20,30 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
+ * 
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {  
+  if (typeof sampleActivity !== 'string'
+    || Number.isInteger(sampleActivity) 
+    || +sampleActivity < 0 
+    || typeof +sampleActivity !== 'number' 
+    || typeof sampleActivity == 'undefined' 
+    || !isNumber(+sampleActivity)) 
+  // if (!(parseInt(sampleActivity) >= 0)) 
+  {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const ln = Math.log(MODERN_ACTIVITY / +sampleActivity);
+
+  if ((Math.ceil(ln / k)) === Infinity || (Math.ceil(ln / k)) < 0) {
+    return false;
+  }
+  return Math.ceil(ln / k);
 }
+
+// console.log(dateSample(' \n\t\r'))
 
 module.exports = {
   dateSample
