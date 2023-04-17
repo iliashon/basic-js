fc6d0af6f111f349c00a09f3b53ec74c35694395
Ilya Tupalski <itupalski@gmail.com>
2023-04-17 20:54:05 +0400

    fix: vigener  (Mon, Apr 17, 2023  8:54:05 PM)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const messageChar = message[i];
+      const keyChar = key[keyIndex];
+
+      if (messageChar.match(/[A-Z]/)) {
+        const messageCharCode = messageChar.charCodeAt(0);
+        const keyCharCode = keyChar.charCodeAt(0);
+        const encryptedCharCode = ((messageCharCode + keyCharCode - 130) % 26) + 65;
+        result += String.fromCharCode(encryptedCharCode);
+
+        keyIndex = (keyIndex + 1) % key.length;
+      } else {
+        result += messageChar;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const encryptedChar = encryptedMessage[i];
+      const keyChar = key[keyIndex];
+
+      if (encryptedChar.match(/[A-Z]/)) {
+        const encryptedCharCode = encryptedChar.charCodeAt(0);
+        const keyCharCode = keyChar.charCodeAt(0);
+        const messageCharCode = ((encryptedCharCode - keyCharCode + 26) % 26) + 65;
+        result += String.fromCharCode(messageCharCode);
+
+        keyIndex = (keyIndex + 1) % key.length;
+      } else {
+        result += encryptedChar;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
