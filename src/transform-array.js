954dc53b356912428bba49bb6e8de1fdd000cbd3
Ilya Tupalski <itupalski@gmail.com>
2023-04-17 17:12:09 +0400

    fix: transform  (Mon, Apr 17, 2023  5:12:09 PM)

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,57 +14,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 
-// function type(value) {
-//   var regex = /^\[object (\S+?)\]$/;
-//   var matches = Object.prototype.toString.call(value).match(regex) || [];
+function type(value) {
+  var regex = /^\[object (\S+?)\]$/;
+  var matches = Object.prototype.toString.call(value).match(regex) || [];
 
-//   return (matches[1] || 'undefined').toLowerCase();
-// }
-// function transform(arr) {
-//   // throw new NotImplementedError('Not implemented');
-//   if (type(arr) !== 'array'){
-//     throw new Error("'arr' parameter must be an instance of the Array!");
-//   }
-//   let doneArr = arr;
-//   if(doneArr.indexOf('--discrard-prev')) {
-//     return doneArr;
-//   }
-//   for(let i = 0; i < doneArr.length; i++){
-//     if(doneArr[i] === '--discard-next'){
-//       if(doneArr[i + 2] === '--double-prev' || doneArr[i + 2] === '--discard-prev') {
-//         doneArr.splice(i, 3);
-//       } else {
-//         doneArr.splice(i, 2);
-//       }
-//     } else if (doneArr[i] === '--discard-prev') {
-//       if(doneArr[i - 2] !== undefined) {
-//         doneArr.splice(i - 1, 2);
-//       } else {
-//         doneArr.splice(i, 1);
-//       }
-//     }
+  return (matches[1] || 'undefined').toLowerCase();
+}
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
+  if (type(arr) !== 'array'){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let doneArr = arr.slice();
+  // if(doneArr.indexOf('--discrard-prev')) {
+  //   return doneArr;
+  // }
+  for(let i = 0; i < doneArr.length; i++){
+    if(doneArr[i] === '--discard-next'){
+      if(doneArr[i + 2] === '--double-prev' || doneArr[i + 2] === '--discard-prev') {
+        doneArr.splice(i, 3);
+      } else {
+        doneArr.splice(i, 2);
+      }
+    } else if (doneArr[i] === '--discard-prev') {
+      console.log('srg')
+      if(doneArr[i - 1] !== undefined) {
+        doneArr.splice(i - 1, 2);
+      } else {
+        doneArr.splice(i, 1);
+      }
+    }
 
-//     if(doneArr[i] === '--double-next'){
-//       if(doneArr[i + 1] !== undefined) {
-//         doneArr.splice(i + 1, 0, doneArr[i + 1]);
-//         doneArr.splice(i, 1);
-//       } else {
-//         doneArr.splice(i, 1);
-//       }
-//     } else if (doneArr[i] === '--double-prev') {
-//       if(doneArr[i - 1] !== undefined) {
-//         doneArr.splice(i - 1, 0, doneArr[i - 1]);
-//         doneArr.splice(i + 1, 1);
-//       } else {
-//         doneArr.splice(i, 1);
-//       }
-//     }
-//   }
-//   return doneArr;
-// }
+    if(doneArr[i] === '--double-next'){
+      if(doneArr[i + 1] !== undefined) {
+        doneArr.splice(i + 1, 0, doneArr[i + 1]);
+        doneArr.splice(i, 1);
+      } else {
+        doneArr.splice(i, 1);
+      }
+    } else if (doneArr[i] === '--double-prev') {
+      if(doneArr[i - 1] !== undefined) {
+        doneArr.splice(i - 1, 0, doneArr[i - 1]);
+        doneArr.splice(i + 1, 1);
+      } else {
+        doneArr.splice(i, 1);
+      }
+    }
+  }
+  return doneArr;
+}
 
 // console.log('--double-next' === '--double-next')
-// console.log(transform(["--discrard-next", "1", "--double-prev", 1]));
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5]));
 
 module.exports = {
   transform
@@ -72,46 +73,46 @@ module.exports = {
 
 
 
-function transform(arr) {
+// function transform(arr) {
 
-  //myLog(arr);
-  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
-  let res = arr.slice();
+//   //myLog(arr);
+//   if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+//   let res = arr.slice();
 
 
-  arr.forEach((el, ind) => {
-    switch (el) {
-      case '--discard-next':
-        if (ind < res.length-1) {
-          res[ind+1] = null;
-          res[ind] = null;
-        } else {
-          res[ind] = null;
-        }
-        break;
-      case '--discard-prev':
-        if (ind > 0) {
-          res[ind-1] = null;
-          res[ind] = null;
-        } else {
-          res[ind] = null;
-        }
-        break;
-      case '--double-next':
-        if (ind < res.length-1) res[ind] = res[ind+1];
-        else {
-          res[ind] = null;
-        }
-        break;
-      case '--double-prev':
-        if (ind > 0)  res[ind] = res[ind-1];
-        else {
-          res[ind] = null;
-        }
-        break;
-    }
-  })
-  console.log(res)
-  return res.filter(el => el !== null);
-}
+//   arr.forEach((el, ind) => {
+//     switch (el) {
+//       case '--discard-next':
+//         if (ind < res.length-1) {
+//           res[ind+1] = null;
+//           res[ind] = null;
+//         } else {
+//           res[ind] = null;
+//         }
+//         break;
+//       case '--discard-prev':
+//         if (ind > 0) {
+//           res[ind-1] = null;
+//           res[ind] = null;
+//         } else {
+//           res[ind] = null;
+//         }
+//         break;
+//       case '--double-next':
+//         if (ind < res.length-1) res[ind] = res[ind+1];
+//         else {
+//           res[ind] = null;
+//         }
+//         break;
+//       case '--double-prev':
+//         if (ind > 0)  res[ind] = res[ind-1];
+//         else {
+//           res[ind] = null;
+//         }
+//         break;
+//     }
+//   })
+//   console.log(res)
+//   return res.filter(el => el !== null);
+// }
 // console.log(transform([ '--double-prev', { foo: 'bar' } ]));
